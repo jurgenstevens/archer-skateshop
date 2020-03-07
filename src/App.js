cafc7c8aba10458f5d620b073521529cc745eaa0
Jurgen <jurgen.stevens@yahoo.com>
2020-03-07 17:18:59 -0600

    Components created and rendered accordingly.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row"></div>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/store" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
     );
   }
 }
