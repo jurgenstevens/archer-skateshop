06302a9e351e8d88bbd7a815b6c7b55f4e2ced8b
Jurgen <jurgen.stevens@yahoo.com>
2020-03-07 21:01:15 -0600

    My cart created and logo size adjusted.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,13 +9,20 @@ export default class Navbar extends Component {
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
-        <ul className="navbar-nav.align-items-center">
+        <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
             <Link to="/" className="nav-link">
               Products
             </Link>
           </li>
         </ul>
+        <Link to="/cart" className="ml-auto">
+          <button>
+            <i className="fas fa-cart-plus" />
+            <br />
+            My Cart
+          </button>
+        </Link>
       </nav>
     );
   }
