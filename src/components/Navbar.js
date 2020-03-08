aaf4fcc7aec2da5bc9780ac50604549ef28a7da4
Jurgen <jurgen.stevens@yahoo.com>
2020-03-07 23:27:06 -0600

    Title has been added to productlist and nav bar color has been changed.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import { ButtonContainer } from "./Button";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
@@ -26,7 +26,16 @@ export default class Navbar extends Component {
             my cart
           </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import Product from "./Product";
+import Title from "./Title";
 
 export default class ProductList extends Component {
+  // need state for the product
+  state = {
+    products: []
+  };
+
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="The" title="Gear" />
+            <div className="row" />
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
