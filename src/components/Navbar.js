e25a405309dee15810d34190213cf6ad2348329b
Jurgen <jurgen.stevens@yahoo.com>
2020-03-07 22:04:47 -0600

    ButtonContainer css has been moved to its own component Button.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.png";
 import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class Navbar extends Component {
   render() {
@@ -29,24 +30,3 @@ export default class Navbar extends Component {
     );
   }
 }
-
-// this is for the button container using styled-components
-const ButtonContainer = styled.button`
-  text-transform: capitalize;
-  font-size: 1.4rem;
-  background: transparent;
-  border: 0.05rem solid var(--lightBlue);
-  color: var(--lightBlue);
-  border-radius: 0.5rem;
-  padding: 0.2rem 0.5rem;
-  cursor: pointer;
-  margin: 0.2rem 0.5rem 0.2rem 0;
-  transition: all 0.5s ease-in-out;
-  &:hover {
-    background: var(--lightBlue);
-    color: var(--mainBlue);
-  }
-  &:focus {
-    outline: none;
-  }
-`;
