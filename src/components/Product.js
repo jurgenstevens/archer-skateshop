1ae234fdf039673e604773a5b9a56773ee3ea5f3
Jurgen <jurgen.stevens@yahoo.com>
2020-03-12 00:28:04 -0500

    Title and price aligned in footer and added color to footer

--- src/components/Product.js
+++ src/components/Product.js
@@ -41,12 +41,13 @@ export default class Product extends Component {
             </button>
           </div>
           {/* this will be the card footer */}
-          <div className="card-footer.d-flex.justify-content-between">
+          <div className="card-footer d-flex justify-content-between">
             {/* name of the product goes here in the footer */}
             <p className="align-self-center mb-0">{title}</p>
             {/* this will be the price in the footer */}
             <h5 className="text-blue font-italic mb-0">
-              <span className="mr-1">${price}</span>
+              <span className="mr-1">$</span>
+              {price}
             </h5>
           </div>
         </div>
@@ -55,4 +56,24 @@ export default class Product extends Component {
   }
 }
 
-const ProductWrapper = styled.div``;
+const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 1s linear;
+  }
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+  }
+  // if cursor is hovering over the card
+  &:hover {
+    .card {
+      border: 0.04rem solid rgba(0, 0, 0, 0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+    }
+    .card-footer {
+      background: rgba(247, 247, 247);
+    }
+  }
+`;
