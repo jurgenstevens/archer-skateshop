d42820aef6e645285c0ec528eed9828c26332531
Jurgen <jurgen.stevens@yahoo.com>
2020-03-15 23:03:15 -0500

    Changed nav bar color to black.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ export default class Navbar extends Component {
 }
 
 const NavWrapper = styled.nav`
-  background: var(--mainBlue);
+  background: black;
   .nav-link {
     color: var(--mainWhite) !important;
     font-size: 1.3rem;
