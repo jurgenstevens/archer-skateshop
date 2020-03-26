55bd05517769113b27222bc879f0f1fb2107b196
Jurgen <jurgen.stevens@yahoo.com>
2020-03-25 21:13:27 -0600

    Cart list displays text of where product will go.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import Title from "../Title";
 import CartColumns from "./CartColumns";
 import EmptyCart from "./EmptyCart";
 import { ProductConsumer } from "../../context";
+import CartList from "./CartList";
 
 export default class Cart extends Component {
   render() {
@@ -19,6 +20,8 @@ export default class Cart extends Component {
                 <React.Fragment>
                   <Title name="your" title="cart" />
                   <CartColumns />
+                  {/* this will display the list of items in the cart */}
+                  <CartList value={value} />
                 </React.Fragment>
               );
               // or else just display the empty cart message
---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function CartItem({ item, value }) {
+  const { id, title, img, price, total, count } = item;
+  const { increment, decrement, removeItem } = value;
+  return <div>Hello from cart item</div>;
+}
