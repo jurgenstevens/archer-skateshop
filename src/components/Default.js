74a5ea542962708d081ff2a8e45ecd26fe429f9c
Jurgen <jurgen.stevens@yahoo.com>
2020-03-26 19:33:54 -0600

    404 page was tweaked and now renders whatever url the user typed in as not found.

--- src/components/Default.js
+++ src/components/Default.js
@@ -2,9 +2,23 @@ import React, { Component } from "react";
 
 export default class Default extends Component {
   render() {
+    console.log(this.props);
     return (
-      <div>
-        <h3>Page Not Found</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="display-3">404</h1>
+            <h1>error</h1>
+            <h1>page not found</h1>
+            <h3>
+              the requested URL{" "}
+              <span className="text-danger">
+                {this.props.location.pathname}
+              </span>{" "}
+              was not found
+            </h3>
+          </div>
+        </div>
       </div>
     );
   }
