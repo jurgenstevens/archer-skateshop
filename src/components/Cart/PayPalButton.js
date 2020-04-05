692a174fc3ce43de9f1a01ddab93e9fba44803a9
Jurgen <jurgen.stevens@yahoo.com>
2020-04-05 17:31:52 -0500

    Added key and value to netlify to delpoy.

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -30,8 +30,7 @@ export default class MyApp extends React.Component {
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox:
-        "AUZd_7LVPlNQayKu-O4ISPJcOwpVRvYMnersB9_W9pO6PPR3z_qTTOR8zc1dwrAA7GrtZ20aXBTOv9SM",
+      sandbox: process.env.REACT_APP_APP_ID,
       production: "YOUR-PRODUCTION-APP-ID",
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
