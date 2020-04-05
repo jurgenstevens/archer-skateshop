6be809367bb0ad6c7fa8201d3c22c24737cfb28e
Jurgen <jurgen.stevens@yahoo.com>
2020-04-05 16:14:47 -0500

    Installed the react express paypal checkout npm.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,24 +8,34 @@ export default class Navbar extends Component {
   render() {
     return (
       <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
-        <Link to="/">
-          <img src={logo} alt="store" className="navbar-brand" />
-        </Link>
-        <ul className="navbar-nav align-items-center">
+        <div className="themed-grid-col">
+          <Link to="/" className="nav-link col-md-4">
+            HOME
+          </Link>
+          <Link to="/">
+            <img
+              src={logo}
+              alt="store"
+              className="navbar-brand col-md-4 text-center"
+            />
+          </Link>
+          <Link to="/cart" className="ml-auto col-md-4">
+            <ButtonContainer>
+              <span className="mr-2">
+                <i className="fas fa-cart-plus" />
+              </span>
+              my cart
+            </ButtonContainer>
+          </Link>
+        </div>
+        {/* this will be added later once we have more links to add besides one */}
+        {/* <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
             <Link to="/" className="nav-link">
               HOME
             </Link>
           </li>
-        </ul>
-        <Link to="/cart" className="ml-auto">
-          <ButtonContainer>
-            <span className="mr-2">
-              <i className="fas fa-cart-plus" />
-            </span>
-            my cart
-          </ButtonContainer>
-        </Link>
+        </ul> */}
       </NavWrapper>
     );
   }
