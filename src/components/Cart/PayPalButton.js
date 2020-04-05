a00ff2c38305c696b18009066950e91a1c285b6a
Jurgen <jurgen.stevens@yahoo.com>
2020-04-05 17:53:07 -0500

    Final paypal adjustments have been made.

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -12,6 +12,8 @@ export default class MyApp extends React.Component {
     const onCancel = (data) => {
       // User pressed "cancel" or close Paypal's popup!
       console.log("The payment was cancelled!", data);
+      this.props.clearCart();
+      this.props.history.push("/");
       // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
     };
 
@@ -24,11 +26,11 @@ export default class MyApp extends React.Component {
 
     let env = "sandbox"; // you can set here to 'production' for production
     let currency = "USD"; // or you can set this value from your props or state
-    let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+    // let total = 1; same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox: "YOUR-SANDBOX-APP-ID",
+      sandbox: process.env.REACT_APP_APP_ID,
       production: "YOUR-PRODUCTION-APP-ID",
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
@@ -43,7 +45,7 @@ export default class MyApp extends React.Component {
         env={env}
         client={client}
         currency={currency}
-        total={total}
+        total={this.props.total}
         onError={onError}
         onSuccess={onSuccess}
         onCancel={onCancel}
